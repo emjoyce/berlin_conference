531a6a8337a0812d8ff3556cc0c6ade01953d3c0
emjoyce <emilybemily55@gmail.com>
2024-06-08 14:37:23 -0700

    adding met shuffle

--- berlin_code/shuffle.py
+++ berlin_code/shuffle.py
@@ -1,79 +1,74 @@
 import pandas as pd
 import numpy as np
-from . import cluster_analysis
+from scipy.cluster import hierarchy
 import random
-import time
+from scipy.spatial.distance import pdist
+
+met_dict = {864691132772384381: 'met7', 864691132656139260: 'met7', 864691132679532125: 'met7',
+            864691132857128518: 'met7', 864691132752141543: 'met7', 864691132635887345: 'met7',
+            864691132679146333: 'met8', 864691132636753137: 'met8', 864691132636263975: 'met8',
+            864691132710743877: 'met8', 864691132724827721: 'met6', 864691132607980867: 'met6',
+            864691132921234401: 'met6', 864691132737950875: 'met6', 864691132681743407: 'met6',
+            864691132685582532: 'met6'}
+
+cluster_dict = {('met7', 'met8'):'m6_only', ('met6', 'met8'):'m7_only',('met6', 'met7'):'m8_only',
+                  ('met8',):'m67_only', ('met7',):'m68_only', ('met6',):'m78_only', ():'all'}
 
 
-def shuffle_clust_hist(df, n = 1000):
+def pull_cluster_sizes(df, metric = 'sokalsneath', threshold = 0, cluster_dict = cluster_dict):
+    distance_matrix = pdist(df.T, metric='sokalsneath')
+    # hierarchical clustering
+    Z = hierarchy.linkage(distance_matrix, method='average')
+    
+    cluster_labels = hierarchy.fcluster(Z, threshold, criterion='distance')
+    #print(cluster_labels)
+    # I wnat this to return the number of each cluster
+    
+    val, count = np.unique(cluster_labels, return_counts=True)
+    clust_count = {}
+    
+    for clust_num, clust_num_count in list(zip(val, count)):
+        #print(clust_num, clust_num_count)
+        # get the first index of that array 
+        
+        clust_idx = np.where(cluster_labels==clust_num)[0][0]
+        #print(f'clust_num:{clust_num}, clust_idx:{clust_idx}')
+        # now get the met types with 0 connections here - should give keys to cluster_dict
+        clust_labels_key = tuple(df.iloc[:,clust_idx][df.iloc[:,clust_idx] == 0].index)
+        #print(f'clust_labels_key:{clust_labels_key}')
+        met_clust = cluster_dict[clust_labels_key]
+        #print(clust_labels_key, met_clust)
+        #print(met_clust, clust_num_count)
+        clust_count[met_clust] = clust_num_count
+
+    # check for met clusters that are not present in clust_count and add them with 0 as value
+    
+    missing_clusts = set(clust_count.keys()) ^ set(cluster_dict.values())
+    if len(missing_clusts) > 0:
+        for(met_clust) in missing_clusts:
+            clust_count[met_clust] = 0
+    
+    return(clust_count)
+
+
+def shuffle_clust_hist(df, pivot_df, n = 1000):
     
     # get real dist 
-    pivot_table_real = pd.pivot_table(df, values='size', index=['l6_group'], columns=['post_pt_root_id'], aggfunc=np.sum, fill_value=0)
-    log_pivot_table_real = np.log1p(pivot_table_real)
-    real_clust_n = cluster_analysis.pull_cluster_sizes(log_pivot_table_real)
-    
+    real_clust_n = pull_cluster_sizes(pivot_df)
     # now shuffle x times! 
     hist_df = pd.DataFrame(columns = real_clust_n.keys())
     
     for i in range(n):
         shuffle_test_df = df.copy()
-        shuffle_test_df['shuffled_l6_group'] = np.random.permutation(shuffle_test_df['l6_group'].values)
-
-        pivot_table_shuffled = pd.pivot_table(shuffle_test_df, values='size', index=['shuffled_l6_group'], columns=['post_pt_root_id'], aggfunc=np.sum, fill_value=0)
-
-        log_pivot_table_shuffled = np.log1p(pivot_table_shuffled)
-
-        new_line = cluster_analysis.pull_cluster_sizes(log_pivot_table_shuffled)
-        hist_df = pd.concat([hist_df,pd.DataFrame([new_line])])
+        # make new shuffle of met dict 
+        shuffled_dict_values = list(met_dict.values())
+        random.shuffle(shuffled_dict_values)
+        shuffled_met_dict = dict(zip(met_dict.keys(), shuffled_dict_values))
+        df['shuffled_met'] = df['pre_pt_root_id'].map(shuffled_met_dict)
+        # make this into a pivot table 
+        df_pivot = df.pivot_table(columns = 'post_pt_root_id', index = 'shuffled_met', values = 'size', fill_value = 0)
+        #recluster
+        new_cluster_sizes = pull_cluster_sizes(df_pivot)
+        hist_df = pd.concat([hist_df,pd.DataFrame([new_cluster_sizes])])
     
     return hist_df
-
-def spatial_shuffle(pre_id, client, max_dist = 5000, compare_num_soma_partners = True, 
-                    nuc_df = None, synapse_res = [9.7,9.7,45], max_retries = 5):
-    '''
-    takes the pre_ids and does a spatial shuffle on all postsyn partners
-    postysyn partners will be dropped if they do not belong to a body with a soma
-    
-    '''
-    soma_df = client.materialize.synapse_query(pre_ids = [pre_id]).reset_index(drop = True)
-    print(f'number of synapses to shuffle for body {pre_id}: {len(soma_df)}')
-
-    if nuc_df is None:
-        nuc_df = client.materialize.query_table('nucleus_detection_v0')
-    
-    if compare_num_soma_partners:
-        len_true_somad_post = len(soma_df[soma_df['post_pt_root_id'].isin(nuc_df['pt_root_id'])])
-        print(f'number of true synapses onto a neuron with a soma: {len_true_somad_post}')
-    
-    # now go through each presyn and get all postsyn partners within max dist
-        
-    for i in range(len(soma_df)):
-        soma_loc = np.vstack(soma_df.loc[i, 'ctr_pt_position'])
-        x, y, z = soma_loc[0][0], soma_loc[1][0], soma_loc[2][0]
-        bounding_box = [[(x-max_dist/synapse_res[0]), (y-max_dist/synapse_res[1]), (z-max_dist//synapse_res[2])],[(x+max_dist/synapse_res[0]), (y+max_dist//synapse_res[0]), (z+max_dist//synapse_res[2])]]
-        # now get all postsyn sites within max_dist
-        # this has been failing from the server side so trying to add a fix so that it will run overnight
-        retry_count = 0
-        success = False
-        while retry_count < max_retries and not success:
-            try:
-                post_syns_in_dist = client.materialize.synapse_query(bounding_box = bounding_box)
-                success = True
-            except:
-                print(f"Attempt {retry_count + 1} for neuron failed")
-                retry_count += 1
-                time.sleep(30)
-        # now pick random post partner from post_syns_in_dist
-        new_post = int(random.choice(list(post_syns_in_dist['post_pt_root_id'])))
-
-        # now update the soma_df with the new post_id
-        soma_df.loc[i, 'shuffled_post_id'] = new_post
-        
-        if i % 25 == 0:
-            print(f'finished {i/len(soma_df)*100}% of synapses')
-    
-    if compare_num_soma_partners:
-        len_shuffled_soma_post = len(soma_df[soma_df['shuffled_post_id'].isin(nuc_df['pt_root_id'])])
-        print(f'number of shuffled synapses onto a neuron with a soma: {len_shuffled_soma_post}')
-    
-    return soma_df
